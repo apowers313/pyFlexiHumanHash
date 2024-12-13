1763e01516e22fa5aeec369ba81b4745506759eb
Adam Powers <apowers@ato.ms>
2024-12-12 21:53:52 -0800

    fix file paths

--- flexihumanhash/dict.py
+++ flexihumanhash/dict.py
@@ -5,6 +5,7 @@ from typing import Callable, TypeVar, Any
 
 registry: dict[str, FlexiDict] = {}
 
+
 class FlexiDict(ABC):
     @abstractmethod
     def get_entry(self, n: int) -> str: ...
@@ -24,7 +25,9 @@ class FlexiDict(ABC):
     def get_registry() -> dict[str, FlexiDict]:
         return registry
 
+
 T = TypeVar("T", bound=type[FlexiDict])
+
 
 def register_dict(name: str) -> Callable[[T], T]:
     def decorator(cls: T) -> T:
@@ -32,6 +35,7 @@ def register_dict(name: str) -> Callable[[T], T]:
         return cls
 
     return decorator
+
 
 class FlexiTextDict(FlexiDict):
     def __init__(self, name: str, words: list[str], min: int = 0, max: int = 2048) -> None:
@@ -53,13 +57,14 @@ class FlexiTextDict(FlexiDict):
         return self.sz
 
     @staticmethod
-    def from_file(name: str, path: str) -> FlexiTextDict:
+    def from_file(name: str, path: Path) -> FlexiTextDict:
         fullpath = Path(path).resolve()
         with open(fullpath, "r") as file:
             lines = file.read().splitlines()
         d = FlexiTextDict(name, lines)
         FlexiDict.register(name, d)
         return d
+
 
 @register_dict("hex")
 class FlexiHexDict(FlexiDict):
@@ -75,8 +80,9 @@ class FlexiHexDict(FlexiDict):
     @property
     def size(self) -> int:
         num_bits = self.sz * 4
-        ret: int = 2 ** num_bits
+        ret: int = 2**num_bits
         return ret
+
 
 @register_dict("decimal")
 class FlexiDecimalDict(FlexiDict):
@@ -84,21 +90,23 @@ class FlexiDecimalDict(FlexiDict):
         self.sz = size
 
     def get_entry(self, n: int) -> str:
-        return f"{n:0{self.sz}d}" 
+        return f"{n:0{self.sz}d}"
 
     @property
     def size(self) -> int:
-        ret: int = 10 ** self.sz
+        ret: int = 10**self.sz
         return ret
 
     def preprocess(self, args: tuple[Any, ...], kwargs: dict[str, Any]) -> FlexiDecimalDict:
         return FlexiDecimalDict(*args, **kwargs)
 
-FlexiTextDict.from_file("noun", "data/build/noun")
-FlexiTextDict.from_file("adj", "data/build/adjective")
-FlexiTextDict.from_file("verb", "data/build/verb")
-FlexiTextDict.from_file("city", "data/build/city")
-FlexiTextDict.from_file("firstname", "data/build/first-name")
-FlexiTextDict.from_file("lastname", "data/build/last-name")
-FlexiTextDict.from_file("femalename", "data/build/female-name")
-FlexiTextDict.from_file("malename", "data/build/male-name")
+
+datadir = Path(__file__) / ".." / ".." / "data" / "build"
+FlexiTextDict.from_file("noun", datadir / "noun")
+FlexiTextDict.from_file("adj", datadir / "adjective")
+FlexiTextDict.from_file("verb", datadir / "verb")
+FlexiTextDict.from_file("city", datadir / "city")
+FlexiTextDict.from_file("firstname", datadir / "first-name")
+FlexiTextDict.from_file("lastname", datadir / "last-name")
+FlexiTextDict.from_file("femalename", datadir / "female-name")
+FlexiTextDict.from_file("malename", datadir / "male-name")
